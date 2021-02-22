29e3953711a2365a68650b2cd3e705f52b6564a0
savanna chiang <savannaac@gmail.com>
2021-02-21 18:20:42 -0600

    Getting Data from Remote Sources lab - complete

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+
+class GetRequester
+
+    attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        request = JSON.parse(self.get_response_body)
+    end
+end
